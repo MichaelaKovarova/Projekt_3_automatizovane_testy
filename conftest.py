28415b4f88dacf2543f288682c9f598b5eeb5089
MichaelaKovarova <misasvecova@email.cz>
2026-06-09 20:41:42 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 def cookies(page):
     page.goto("https://www.seznam.cz/")
 
-    cookie_banner = page.locator("#cwl-main")
+    cookie_banner = page.locator('[data-testid="cwl-dialog-headline"]')
     cookie_banner.click()
 
     button_refuse = page.locator("text='Souhlasím'")
